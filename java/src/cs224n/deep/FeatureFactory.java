589b591869807adb0ed37310e2fcaebc337bef26
Benoît <benoitdancoisne@gmail.com>
2014-11-24 19:15:45 -0800

    built the methods 'initializeVocab' and 'readWordVectors'

--- java/src/cs224n/deep/FeatureFactory.java
+++ java/src/cs224n/deep/FeatureFactory.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+
 import org.ejml.simple.*;
 
 
@@ -45,35 +46,57 @@ public class FeatureFactory {
 			Datum datum = new Datum(word, label);
 			data.add(datum);
 		}
-
+		in.close();
 		return data;
-	}
- 
+	} 
  
 	// Look up table matrix with all word vectors as defined in lecture with dimensionality n x |V|
 	static SimpleMatrix allVecs; //access it directly in WindowModel
 	public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
-		if (allVecs!=null) return allVecs;
-		return null;
-		//TODO implement this
-		//set allVecs from filename		
-
+		if (allVecs!=null)
+			return allVecs;
+		List<double[]> words = new ArrayList<double[]>();
+		BufferedReader in = new BufferedReader(new FileReader(vecFilename));
+		for (String line = in.readLine(); line != null; line = in.readLine()) {
+			if (line.trim().length() == 0) {
+				continue;
+			}
+			String[] word = line.split("\\s+");
+			int n = word.length; // dim of the word vectors
+			double[] word_data = new double[n];
+			for (int i = 0; i < n; i++){
+				word_data[i] = Double.parseDouble(word[i]);
+			}
+			words.add(word_data);
+		}
+		in.close();		
+		double[][] matrix = new double[words.size()][];
+		matrix = words.toArray(matrix);
+		return new SimpleMatrix(matrix);
 	}
+	
 	// might be useful for word to number lookups, just access them directly in WindowModel
 	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>(); 
 	public static HashMap<Integer, String> numToWord = new HashMap<Integer, String>();
 
 	public static HashMap<String, Integer> initializeVocab(String vocabFilename) throws IOException {
-		//TODO: create this
+		if (!wordToNum.isEmpty()){
+			System.out.println("already done");
+			return wordToNum; }
+		BufferedReader in = new BufferedReader(new FileReader(vocabFilename));
+		int index = 0;
+		for (String line = in.readLine(); line != null; line = in.readLine()) {
+			if (line.trim().length() == 0) {
+				continue;
+			}
+			String[] bits = line.split("\\s+");
+			String word = bits[0];
+			wordToNum.put(word,index);
+			numToWord.put(index, word);
+			index++;
+		}
+		in.close();
+    	System.out.println("done");
 		return wordToNum;
-	}
- 
-
-
-
-
-
-
-
-
+	}	
 }
