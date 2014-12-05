f013d72448677de0be8a163250e7810a26299ebd
Benoît <benoitdancoisne@gmail.com>
2014-12-05 15:43:50 -0800

    visualization script for word vectors

---
+++ visu.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  5 11:38:58 2014
+
+@author: benoit
+"""
+import numpy as np
+import pylab as pl
+import sys
+import tsne
+
+if len(sys.argv) < 3:
+    print 'Usage:'
+    print '  python %s <saved-vocab.csv> <output.png>' % sys.argv[0]
+    exit()
+
+X = np.loadtxt(sys.argv[1], skiprows=1)
+Y = tsne.tsne(X, 2, 50, 20.0);
+pl.scatter(Y[:,0], Y[:,1], 20);
+pl.savefig(sys.argv[2], bbox_inches='tight')
